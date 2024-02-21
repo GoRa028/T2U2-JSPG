18133ab021069e5c0b292180afd036267f46e942
Pablo GoRa <wgoramarsola06@gmail.com>
2024-02-21 16:16:38 -0600

    Modified Main

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Hello and welcome!\n");
+        System.out.println("Hello world");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 3; i++) {
             System.out.println("i = " + i);
         }
     }
