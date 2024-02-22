dfc84976c4684b7839a416a6acaac44100d618dd
Pablo GoRa <wgoramarsola06@gmail.com>
2024-02-21 20:49:47 -0600

    Se añadió la clase Rectángulo

---
+++ src/Rectangulo.java
@@ -0,0 +1,13 @@
+public class Rectangulo {
+    int b, h;
+
+    public Rectangulo(int base, int altura) {
+        b = base;
+        h = altura;
+    }
+
+    public double area (int base, int altura) {
+        double res;
+        return res = base*altura;
+    }
+}
